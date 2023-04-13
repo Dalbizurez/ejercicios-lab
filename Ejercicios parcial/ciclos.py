485aad8b849d4190cc72191ff5a3389f9478dd66
Dalbizurez <dalbizurez@correo.url.edu.gt>
2023-04-13 08:15:10 -0600

    Create ciclos.py

---
+++ Ejercicios parcial/ciclos.py
@@ -0,0 +1,19 @@
+suma = 0
+for x in range(0,101):
+    suma += x
+print(suma)
+
+suma = 0
+i = 0
+while i < 100:
+    i += 1
+    suma += i
+print(suma)
+
+suma = 0
+i = 0
+while True:
+    i += 1
+    suma += i 
+    if i == 100: break
+print(suma)
